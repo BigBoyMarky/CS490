59798e6a9aa9d4cdf76c82959a5229d1e504942d
markwyang <markyang13705@gmail.com>
2015-02-13 21:37:47 -0500

    Create MyRunnable.java

---
+++ MyRunnable.java
@@ -0,0 +1,17 @@
+public class MyRunnable implements Runnable {
+
+    public void run(){
+       while(true){
+    	   try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	   
+    	   //for 
+    	   
+    	   
+       }
+    }
+  }
