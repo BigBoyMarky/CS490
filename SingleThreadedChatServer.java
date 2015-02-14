785a68db1724091d1b25b2c8304c9888764c87b5
markwyang <markyang13705@gmail.com>
2015-02-14 01:12:39 -0500

    Update SingleThreadedChatServer.java

--- SingleThreadedChatServer.java
+++ SingleThreadedChatServer.java
@@ -1,131 +1,76 @@
-import java.io.*;
-import java.net.*;
 import java.util.*;
-import java.lang.Object;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.net.*;
+import java.io.*;
 
-public class SingleThreadedChatServer {
-	
+	//Create a server based on command line input, DONE
+	//server waits for clients to connect, DONE
+	//once the clients connect, it sends acknowledgement back, adds clinet to a list.
+		//it then sends an update to all previously connectd clients about the new user
+	//it regularly checks for heartbeats. If heartbeat not received, it will assume it's dead and remove it from the list.
+	//the client can initiate a chat with another client
+public class SingleThreadedChatServer// implements Runnable
+{
+	private long heartbeat_rate = 4000;//in milliseconds
+	ServerSocket serverSocket;
 	static SingleThreadedChatServer server;
-	private ServerSocket serverSocket = null;
-	private int heartbeat_rate;
-	static Thread myThread;
-	
-	private Vector<Client> connectedClients = new Vector<Client>();
-	
 	public static void main(String[] args) throws IOException
-    {
-        if(args.length > 0)
-        {
-            try
-            {
-                int port = Integer.parseInt(args[0]);
-                if(port >=1025 && port <= 65535)
-                {
-                    server = new SingleThreadedChatServer(port);
-                    myThread = new Thread(server);
-                }
-                else
-                {
-                    System.out.println("Port number is out of range. Try a port between 1025 and 65535. This program will end.");
-                    return;
-                }
-            }
-            catch(Exception e)
-            {
-                System.out.println("Invalid input. Please specify the port number with integers. Also make sure the port is between 1025 and 65535");
-            }
-        }
-        else
-        {
-            server = new SingleThreadedChatServer();
-            System.out.println("Port not specified. Using free port " + server.getLocalPort());
-            myThread = new Thread(server);
-        }
-        myThread.start();
-    }
-	
-	 public SingleThreadedChatServer(int port) throws IOException 
-	 {
-	        serverSocket = new ServerSocket(port);
-	        serverSocket.setReuseAddress(true);
-	 }
-	
-	 public SingleThreadedChatServer() throws IOException 
-	 {
-	        serverSocket = new ServerSocket(0);
-	        serverSocket.setReuseAddress(true);
-	 }
-	 
-	
-	
-	void run () {
-		
-
-		
-		while ( true ) {
-			Socket socket = serverSocket.accept();			
-			PrintWriter print = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            
-            String msg = reader.readLine();
-            
-            Pattern p = Pattern.compile("register<*,*,*>");
-            
-            Matcher m = p.matcher(msg);
-            boolean b = m.matches();
-            
-            if (b) {
-            	;	
-            }
-            
-            else if( msg.equals("heartbeat")) {
-            	
-            }
-            
-            else if( msg.equals("get")){
-            	
-            }
-            else {
-            	System.out.println("Invalid message");
-            }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-       }
-            /*
-			while ( true ) {
-				receive message m from client c;
-				if (m == register <name , ip , port >)
-				// process registration , and send an a c k n o w l e d g e m e n t / failure to c
-				else if (( m == heartbeat <name >)
-				if ( previous heartbeat < name > was received less than
-				heartbeat_rate seconds ago )
-				// keep client c alive
-				else if (m == get )
-				// send G to client c
+	{
+		if(args.length > 0)
+		{
+			try
+			{
+				int port = Integer.parseInt(args[0]);
+				if(port >= 1025 && port <= 65535)
+					server = new SingleThreadedChatServer(port);
 				else
-				// invalid message from client c , server closes connection ;
+				{
+					System.out.println("Port is out of range. Try a port between 1025 and 65535. This program will close.");
+					return;
 				}
 			}
-		*/
-		
-		
-		
-		while (true) {
-		
-			Message fromClient = (Message)input.readObject();
-			
+			catch(Exception e)
+			{
+					System.out.println("Please enter valid input. We want integers only!");
+			}			
 		}
-		
+		else
+		{
+			server = new SingleThreadedChatServer(0);//if none was specified, uses 0, which locates default
+			System.out.println("Port was not specified. Using free port " + server.serverSocket.getLocalPort());		
+		}
+		server.runServer();
 	}
-	
+
+	public SingleThreadedChatServer(int port) throws IOException
+	{
+		serverSocket = new ServerSocket(port);
+		serverSocket.setReuseAddress(true);
+	}
+
+	public void runServer()
+	{
+		ArrayList<ClientObject> clientList = new ArrayList<ClientObject>();
+		try
+		{
+			while(true)
+			{
+				Socket socket = serverSocket.accept();
+				PrintWriter printer = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				String message = reader.readLine();
+				clientList.add(new ClientObject(message, request.getRemoteAddr(), socket, heartbeat_rate));
+				//'0' = getList(); '1' = heartbeat; '2' = ;
+/*				String message = reader.readLine();
+				if(message.equals("0"))
+					getList();
+				if(message.equals("1"))
+					verifyHeartbeat();
+*/			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
 }
