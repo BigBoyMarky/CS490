a386930e996f34c142210b2c664c27dfc13d17f1
Charlie Su <dragonsmirth@gmail.com>
2015-02-23 23:41:23 -0500

    Quickfix to SingleThreadedChatServer and updated dummycleint

--- src/DummyClient.java
+++ src/DummyClient.java
@@ -6,19 +6,28 @@ import java.net.UnknownHostException;
 public class DummyClient implements Runnable
 {
 	static String name = "a";
+	static int port = 0;
 	public static void main(String[] args)
 	{
-		for (int i = 0; i < 10; i++) {
+		try
+		{
+			port = Integer.parseInt(args[0]);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Need to enter port of the server!");
+		}
+		for (int i = 0; i < 10000; i++) {
 			name+=i;
 			System.out.printf("index:%d:%s\n",i,name);
-			try{Thread.sleep(100);}catch(InterruptedException e) { Thread.currentThread().interrupt();}
+			try{Thread.sleep(100);}catch(InterruptedException e) {Thread.currentThread().interrupt();}
 			new Thread(new DummyClient()).start();
 			name = "a";	
 		}
 	}
 	public void run()
 	{
-		ChatClient dummyClient = new ChatClient("localhost", 5620, name);	
+		ChatClient dummyClient = new ChatClient("localhost", port, name);	
 			//System.out.println(name);
 		String[] sucks = {};
 		try
