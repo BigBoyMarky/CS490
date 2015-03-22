c92a6d9ccec3b3946263d5d73eef5f1485fe8b65
Gott Phusit <saranyu_phusit@hotmail.com>
2015-03-22 19:23:33 -0400

    Create ReliableBroadcaster.java

---
+++ assignment2/edu/purdue/cs490/ReliableBroadcaster.java
@@ -0,0 +1,56 @@
+package edu.purdue.cs490;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class ReliableBroadcaster implements ReliableBroadcast {
+	
+	private Process currentProcess;
+	private BroadcastReceiver receiver;
+	private ConcurrentSkipListSet<Process> members;
+	private BEBroadcaster beblayer;
+	private ConcurrentSkipListSet<Message> receivedMessage;
+	
+	public ReliableBroadcaster(Process currentProcess, BroadcastReceiver br){
+		init(currentProcess, br);
+		
+	}
+	
+	public void init(Process currentProcess, BroadcastReceiver br){
+		this.currentProcess = currentProcess;
+		this.receiver = br;
+		beblayer = new BEBroadcaster(currentProcess, this);
+	}
+	
+	public void addMember(Process member){
+		members.add(member);
+	}
+	
+	public void removeMember(Process member){
+		members.remove(member);
+	}
+	
+	public void rbroadcast(Message m) {
+		m.setSender(currentProcess);
+		beblayer.BEBroadcast(m);
+	}
+	
+	public Message receive(Message pre) {
+		Message m = beblayer.receive(pre);
+		if(m!=null)
+			receivedMessage.add(m);
+		return null;
+	}
+
+	public void failHandler(List<Process> lostmembers){
+		for(Message m : receivedMessage){
+			for(Process lost: lostmembers ){
+				if(m.getSender()==lost){
+					Message newMessage = new ChatClientMessage(currentProcess, 0, m.getMessageContents(), 1);
+					rbroadcast(newMessage);
+				}
+			}
+		}
+	}
+}
